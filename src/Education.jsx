661401e957dc4b295bd7bd58a6263e941cd4ec9c
rares1215 <126170459+rares1215@users.noreply.github.com>
2024-03-11 17:24:14 +0200

    Update Education.jsx

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,5 +1,5 @@
 import ProgressBar from "react-bootstrap/ProgressBar";
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import  ProjectPreview  from "./Projectpreview.jsx";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
